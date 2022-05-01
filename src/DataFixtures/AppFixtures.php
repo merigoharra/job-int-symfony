2c3c33e1fd7a8d59a637b5f6b0146a8efd24a792
Henini Mohamed <henini.mohamed@outlook.fr>
2022-05-01 06:05:56 +0200

    Added password hashing, added commentary

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -8,30 +8,44 @@ use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class AppFixtures extends Fixture
-{
-    public function load(ObjectManager $manager): void
-    {
+class AppFixtures extends Fixture {
+
+    /*
+     * Use for user password hashing
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordHasherInterface $encoder) {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager): void {
+
         $faker = Factory::create('fr_FR');
 
         /*
          * Creating fixtures for data testing
          * (note: I got some troubles for the reviews I'll gratefully talk about that during interview about the best way to link a review to a random user, so I just put all created reviews only for current created user)
          */
+        //Create Fake User
         for ($u=0; $u<30; $u++){
             $user = new User();
+            $hash = $this->encoder->hashPassword($user, $faker->password);
             $user->setEmail($faker->email)
                  ->setFirstName($faker->firstName)
                  ->setLastName($faker->lastName)
-                 ->setPassword($faker->password);
+                 ->setPassword($hash);
             $manager->persist($user);
+            // Create fake story
             for ($s=0; $s<mt_rand(3, 7); $s++ ){
                 $story = new Stories();
                 $story->setUser($user)
                        ->setTitle($faker->sentence(6, true))
                        ->setContent($faker->realText(mt_rand(50, 200)));
                 $manager->persist($story);
+                // Create fake Review
                 for($r=0; $r< mt_rand(1,10); $r++){
                     $review = new Reviews();
                     $review->setContent($faker->sentence(mt_rand(1,7)))
