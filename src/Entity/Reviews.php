83364ae3f9e7800efe4e6dc7c2f35b1425ab0e22
Henini Mohamed <henini.mohamed@outlook.fr>
2022-05-02 03:11:42 +0200

    Add apiplatform ressources and storiesrpository

--- src/Entity/Reviews.php
+++ src/Entity/Reviews.php
@@ -4,9 +4,11 @@ namespace App\Entity;
 
 use App\Repository\ReviewsRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
  * @ORM\Entity(repositoryClass=ReviewsRepository::class)
+ * @ApiResource
  */
 class Reviews
 {
